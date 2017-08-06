8054cf6f05530f398415a65ce11385d23cd39b61
tonkytonky <as@asamsonov.me>
2017-08-06 16:23:44 +0300

    Fix code to work correctly for all the cases

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,9 +3,17 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
-    if discriminant == 0:
+
+    if discriminant > 0:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
+        return root1, root2
+    elif discriminant == 0:
+        root1 = (-b / (2 * a))
         return root1, None
     else:
-        return root1, root2
+        return None, None
+
+
+if __name__ == '__main__':
+    print(get_roots(*(int(number) for number in input().split(' '))))
